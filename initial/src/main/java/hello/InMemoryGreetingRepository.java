89d57b5ac0cbee3afebe2573dc4c2c9db4fb8312
rubiconRiver <garett.gelsinger@gmail.com>
2019-04-01 20:14:51 -0600

    Implemented save function

--- initial/src/main/java/hello/InMemoryGreetingRepository.java
+++ initial/src/main/java/hello/InMemoryGreetingRepository.java
@@ -2,10 +2,15 @@ package hello;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class InMemoryGreetingRepository implements GreetingRepository {
+    private final List<Greeting> greetings = new ArrayList<>();
+
     @Override
     public void save(Greeting greeting) {
-
+        greetings.add(greeting);
     }
 }
