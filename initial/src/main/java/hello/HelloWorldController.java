b72ef406174561b8ce4ebac8d13a857b7d4033f8
rubiconRiver <garett.gelsinger@gmail.com>
2019-04-23 18:37:50 -0600

    fixed CORS issue

--- initial/src/main/java/hello/HelloWorldController.java
+++ initial/src/main/java/hello/HelloWorldController.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-
+@CrossOrigin(origins = "*")
 @Controller
 public class HelloWorldController {
     private final GreetingService greetingService;
